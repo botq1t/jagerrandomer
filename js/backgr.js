e6450d114b8b4f29060865b8836b6c0e6118e11c
botq1t <77788765+botq1t@users.noreply.github.com>
2021-08-07 11:49:53 +0300

    Add files via upload

--- js/backgr.js
+++ js/backgr.js
@@ -1,11 +1,5 @@
-let photo=[
-		"images/yager.jpg",
-		"images/yager2.jpg",
-		"images/yager3.jpg",
-		"images/yager4.jpg",
-		"images/yager5.jpg",
-		];
-		var ph=photo.length;
-		var phh=Math.round(Math.random()*(ph-1));
-		console.log("Background: "+ph, phh)
-		document.write("<style type=\"text/css\">body {background-size: 25% 25%; background-image: url("+photo[phh]+"); background-attachment: fixed;</style>");
+var ph=5;
+var phh=Math.round(Math.random()*(ph-1));
+
+document.write("<style type=\"text/css\">body {background-size: 25% 25%; background-image: url(images/yager/yager"+phh+".jpg); background-attachment: fixed;</style>");
+console.log("Background: "+ph, phh);
